37724baf466bac2f11021aa3ab669aa3f34e045b
SebastianMettes <sebastian.mettes@gmail.com>
2021-01-21 15:45:17 -0700

    setup classes in DLAgent

--- Docker/agent/DLAgent.py
+++ Docker/agent/DLAgent.py
@@ -1,11 +1,59 @@
-def calc_action(agent_id,state):
-    #Will need to receive complete state data
-    #check for updates
+import os
+class agent():
+    def __init__(self):
+        pass
 
-    return [1,1] #return action
+    def calc_action(self,agent_version,state):
+        #Will need to receive complete state data (state)
+        #receive agent ID value for Deep Network weights
+        #check for updates
 
-def update_agent():#check folder for most recent updates
+        return [1,1] #return action
 
-    agent = "1.000"
-    return(agent)
+    def update_version(self,agent_path):#determine most recent agent release folder 
+        i = 1
+        while os.path.isdir(os.path.join(agent_path,str(i))):
+            i+=1
+        i -=1 
+        return(i)
     
+
+#class DLAgent(agent):
+#    def __init__(self):
+#        super().__init__()
+
+
+#    def normalize_action(self, action):
+#        return normalize(action)
+
+#    def calc_action(self,state):
+        #call DL network on state
+#        action  = self.normalize_action(self.network(state))
+#        return action
+    
+#    def update_version(self):
+#        pass
+
+#class BAgent(agent):
+#    pass
+
+#class QAgent(agent):
+#    def __init__(self):
+#        pass
+
+
+#def doagentstuff(a:agent):
+#    a.calc_action(s)
+#    a.update_version()
+
+#dlagent = DLAgent()
+
+#doagentstuff(dlagent)
+
+#qagent = QAgent()
+
+#doagentstuff(qagent)
+
+#bb = BAgent()
+
+#bb.cal
--- Docker/launchenv.py
+++ Docker/launchenv.py
@@ -1,5 +1,5 @@
 from gym_multiarm.envs import multi_arm_env as env
-from agent.DLAgent import calc_action, update_agent
+from agent.DLAgent import agent
 from datetime import datetime
 import random
 import math
@@ -10,54 +10,69 @@ import json
 import os
 import uuid
 
-#Setup Environment
-#import config file
 
+##Create a unique ID for host computer
 host_id = uuid.uuid4()
 
-#load config.json here... 
+##load config.json here... 
 with open("/data/sim/config.json","r") as file:
     config=json.load(file)
 
-
-#steel = env.Multi_armMaterial("steel",2.15E9,0.5,8050)
+##Import materials
 steelprop = config['materials']['steel']
 steel = env.Multi_armMaterial('steel',steelprop['E'],steelprop['poisson'],steelprop['density'])
 plaprop = config['materials']['pla']
 pla = env.Multi_armMaterial('pla',plaprop['E'],plaprop['poisson'],plaprop['density'])
 
+##set arm length (all arms are identical)
 arm_length = config['dimensions']["arm_length"]
+
+##set number of simulation steps per simulation
 numSteps = config['num_steps']
 
+#Setup Environment object
 environmentTest = env.Multi_armEnv()
 environmentTest.reset(False,True,config['dimensions']['arm_width'],config['dimensions']['arm_height'],arm_length,arm_length,steel,config['step_size'],config['max_torque'],[1.0,1.0])
 
-
+#Create agent object
+action_agent = agent()
 
 
 while True:
     #check for new weights
-    agent_id = update_agent()
-    state_tensor = []
+    agent_version = action_agent.update_version(config["agent_path"])
+
+    state_tensor = [] #initialize an empty state tensor
+
     #set the target from random coordinates:
     target_angle = (random.random()*2*math.pi)
     target_radius = random.random()*2*arm_length
     target = [target_radius*math.sin(target_angle),target_radius*math.cos(target_angle)]
+
+    #reset the environment to starting state
     environmentTest.reset(False,True,0.2,0.125,1.0,1.0,pla,0.001,1,target)
+
+    #get initial state 
     state = environmentTest.getstate()
+
+    #get the time as part of the unique output data file.
     now = datetime.now()
     date_time = now.strftime("%Y%m%d%H%M%S")
+
+    #create the filename, path, for the output data file
     filename = str(host_id)+"."+date_time+".JSON"
-    filename = os.path.join(config["save_dir"],"trial",agent_id,filename)
+    filename = os.path.join(config["save_dir"],str(agent_version),filename)
+
+    #conduct simulation
     for i in range(numSteps):
-        #environmentTest.render()
-        action = calc_action(agent_id,state_tensor) 
-        state,state_new,action = environmentTest.forwardStep([action[0],action[1]])
-        reward = environmentTest.reward()
-        state_tensor.append((state,state_new,action,reward))
+        #environmentTest.render() #not applicable to slave machines.
+        action = action_agent.calc_action(agent_version,state_tensor) #use agent to determine action from current state and agent version
+        state,state_new,action = environmentTest.forwardStep([action[0],action[1]]) #run simulation
+        reward = environmentTest.reward() #calculate reward
+        state_tensor.append((state,state_new,action,reward)) #append information to state_tensor
     
     with open(filename,"w") as file:
-        file.write(json.dumps(state_tensor,indent=0))
+        file.write(json.dumps(state_tensor,indent=0)) #save state_tensor for agent optimization
         print("File Saved")
     
 
