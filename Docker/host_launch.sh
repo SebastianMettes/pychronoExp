dd2d8301befbf58c79015531d3ad32c1e93c0e78
sebastian mettes <sebastian.mettes@gmail.com>
2021-02-12 23:06:29 +0000

    modified config file

--- Docker/host_launch.sh
+++ Docker/host_launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker run --gpus all --cpus="2" -v /data/sim:/data/sim pychronohost:latest
+sudo docker run --gpus all --cpus="2" -d -v /data/sim:/data/sim pychronohost:latest
 
