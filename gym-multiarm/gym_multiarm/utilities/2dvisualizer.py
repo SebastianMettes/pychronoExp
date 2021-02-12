cf6ef9b88d35ac1401cc3e2b534ad9b5cc77a13f
SebastianMettes <sebastian.mettes@gmail.com>
2021-02-12 16:57:44 -0700

    added action visualization capabilities to visualizer utility

--- gym-multiarm/gym_multiarm/utilities/2dvisualizer.py
+++ gym-multiarm/gym_multiarm/utilities/2dvisualizer.py
@@ -4,14 +4,14 @@ import os
 import json
 from tqdm.auto import tqdm
 
-top = '/home/sebastian/Documents/non_random_starts/'
+top = '/home/sebastian/Documents/2.2 random_starts_repeater/'
 folder = os.path.join(top,'episodes')
 lossdata = os.path.join(top,'agent/data.csv')
 with open(os.path.join(top,'config.json'),"r") as file:
     config=json.load(file)
 
-agent = 20
-episode_number = 92
+agent = 9
+episode_number = 40
 percentile = config['PERCENTILE']
 num_steps = config['num_steps']
 
@@ -29,7 +29,7 @@ episodes = sorted(episodes,key=lambda x: x[0])
 
 
 data = []
-#extract x,y coordinate data
+#extract x,y coordinate data of arm 1
 for i in tqdm(range(len(episodes))):
     EpisodeX = []
     print(i)
@@ -39,7 +39,7 @@ for i in tqdm(range(len(episodes))):
         EpisodeX.append((posX,posY))
     data.append(EpisodeX)
 data1 = []
-#extract x,y coordinate data
+#extract x,y coordinate data of arm 0
 for i in tqdm(range(len(episodes))):
     EpisodeX = []
     print(i)
@@ -49,15 +49,15 @@ for i in tqdm(range(len(episodes))):
         EpisodeX.append((posX,posY))
     data1.append(EpisodeX)
 actiondata = []
-#extract x,y coordinate data
+#extract x,y coordinate data of actions
 try:
     for i in tqdm(range(len(episodes))):
         EpisodeX = []
         print(i)
         for j in range(0,num_steps):
-            posX = episodes[i][1][j][6]
-            posY = episodes[i][1][j][7]
-            EpisodeX.append((posX,posY))
+            action0 = episodes[i][1][j][6]
+            action1 = episodes[i][1][j][7]
+            EpisodeX.append((action0,action1))
         actiondata.append(EpisodeX)
 except Exception as e:
     print('no action data found')
@@ -77,24 +77,23 @@ except Exception as e:
 
 plt_data = data[episode_number]
 plt_data1 = data1[episode_number]
+pltAction = actiondata[episode_number]
 xcoords,ycoords = zip(*plt_data)
 xcoords1,ycoords1 = zip(*plt_data1)
+action0,action1 = zip(*pltAction)
+steps = []
+for i in range(len(action0)):
+    steps.append(i)
 
 fig,ax = plt.subplots()
-
-
 targetx = episodes[episode_number][int(1)][int(0)][int(4)]
 targety = episodes[episode_number][int(1)][int(0)][int(5)]
-
-#import pdb; pdb.set_trace()
-
 plt.plot(xcoords,ycoords)
 plt.plot(xcoords1,ycoords1)
 sx1,sy1 = xcoords[-1],ycoords[-1]
 sx0,sy0 = xcoords1[-1],ycoords1[-1]
-
-plt.xlim(-2,2)
-plt.ylim(-2,2)
+plt.xlim(-2.2,2.2)
+plt.ylim(-2.2,2.2)
 plt.plot([0,sx0],[0,sy0],label='arm1')
 plt.plot([sx0,sx1],[sy0,sy1],label='arm2')
 plt.title(f"Episode {episode_number} -- Reward : {episodes[episode_number][0]}\nTarget: [{targetx},{targety}]")
@@ -103,4 +102,12 @@ plt.legend()
 ax.add_patch(circle1)
 
 plt.show()
+
+print('now for action plots')
+fig,ax = plt.subplots()
+plt.plot(steps,action0,steps,action1)
+plt.title(f"Episode {episode_number} -- Reward : {episodes[episode_number][0]}\nTarget: [{targetx},{targety}],actions")
+plt.show()
+
+
 
--- gym-multiarm/gym_multiarm/utilities/extractor.py
+++ gym-multiarm/gym_multiarm/utilities/extractor.py
@@ -46,5 +46,5 @@ if __name__=="__main__":
     with open("/data/sim/config.json","r") as file:
         config=json.load(file)
 
-    extractor(1,10,config,'/home/sebastian/Documents/random_starts_new/episodes',skip=3)
+    extractor(6,10,config,'/home/sebastian/Documents/2.2 random_starts_repeater/episodes',skip=1)
 
