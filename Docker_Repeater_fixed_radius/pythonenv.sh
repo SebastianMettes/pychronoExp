f50e71540208b7f3b2ad35246d33876f13af46f9
SebastianMettes <sebastian.mettes@gmail.com>
2021-02-17 12:14:28 -0700

    new docker container

---
+++ Docker_Repeater_fixed_radius/pythonenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Hello
+cd /pychronoExp 
+git pull
+cd /
+#/root/miniconda/bin/conda activate 379
+python --version
+#python -c "print('hello world')"
+#python -c "conda activate 379"
+python "/pychronoExp/Docker_Repeater_fixed/node_env_launch.py"
