390e6b080f94bc5594ee4a5ecebdd86ebc3cf5d4
sebastian mettes <sebastian.mettes@gmail.com>
2021-02-20 18:05:47 +0000

    auto launch 4 containers

--- Docker/launch.sh
+++ Docker/launch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo docker run --cpus="4" -d -v /data/sim:/data/sim pychrono:latest
-sudo docker run --cpus="5" -d -v /data/sim:/data/sim pychrono:latest
-sudo docker run --cpus="5" -d -v /data/sim:/data/sim pychrono:latest
+sudo docker run --cpus="4" -d -v /data/sim:/data/sim sebastianmettes/pychrono-fea-radius
+sudo docker run --cpus="4" -d -v /data/sim:/data/sim sebastianmettes/pychrono-fea-radius:latest
+
+sudo docker run --cpus="4" -d -v /data/sim:/data/sim sebastianmettes/pychrono-fea-radius:latest
+
+sudo docker run --cpus="4" -d -v /data/sim:/data/sim sebastianmettes/pychrono-fea-radius:latest
+
 
