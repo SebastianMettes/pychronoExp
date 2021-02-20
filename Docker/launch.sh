3c9cea99e3bc29cd9932fe228be6779c0b773e62
SebastianMettes <sebastian.mettes@gmail.com>
2021-02-20 11:35:38 -0700

    mount networks drives to two machiens on the network

--- Docker/launch.sh
+++ Docker/launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo docker run --cpus="4" -d -v /data/sim:/data/sim sebastianmettes/pychrono-fea-radius
-sudo docker run --cpus="4" -d -v /data/sim:/data/sim sebastianmettes/pychrono-fea-radius:latest
+sudo docker run --cpus="4" -d -v /data/sim:/data/sim -v /data/cold:/data/cold sebastianmettes/pychrono-fea-radius:latest
+sudo docker run --cpus="4" -d -v /data/sim:/data/sim -v /data/cold:/data/cold sebastianmettes/pychrono-fea-radius:latest
 
-sudo docker run --cpus="4" -d -v /data/sim:/data/sim sebastianmettes/pychrono-fea-radius:latest
+sudo docker run --cpus="4" -d -v /data/sim:/data/sim -v /data/cold:/data/cold sebastianmettes/pychrono-fea-radius:latest
 
-sudo docker run --cpus="4" -d -v /data/sim:/data/sim sebastianmettes/pychrono-fea-radius:latest
+sudo docker run --cpus="4" -d -v /data/sim:/data/sim -v /data/cold:/data/cold sebastianmettes/pychrono-fea-radius:latest
 
 
